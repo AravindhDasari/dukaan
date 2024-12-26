8f808e4159559a7e3f6a1b66c52f2a88116ec8b7
Aravindh-dasari <dasariaravindh21@outlook.com>
2024-12-27 00:57:22 +0530

    Created Wrapper for User Class to perform User operations on the User

---
+++ src/main/java/com/dukaan/userservice/config/security/CustomUserDetails.java
@@ -0,0 +1,81 @@
+package com.dukaan.userservice.config.security;
+
+import com.dukaan.userservice.domain.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+//Wrapper for User Class to perform User operations on the User
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        List<SimpleGrantedAuthority> authorities = user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getRoleName()))
+                .collect(Collectors.toList());
+        System.out.println("Log Authorities of current User: "+authorities);
+        return authorities;
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public String getUsername() {
+        return user.getUserName();
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
