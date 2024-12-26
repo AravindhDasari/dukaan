f4a951be99b750e46de9676c69f8b94d4304e16d
Aravindh-dasari <dasariaravindh21@outlook.com>
2024-12-27 01:01:19 +0530

    Modifying Roles to intialise Empty HAshset whenever NoArgs Constructor is called for Role()

--- src/main/java/com/dukaan/userservice/domain/model/Role.java
+++ src/main/java/com/dukaan/userservice/domain/model/Role.java
@@ -5,10 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class Role {
 
     @Id
@@ -17,5 +19,21 @@ public class Role {
 
     @Column(unique = true, nullable = false)
     private String roleName;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "roles_privileges",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "privilege_id")
+    )
+    private Set<Privilege> privileges;
+
+    // Constructor to initialize privileges
+    public Role() {
+        this.privileges = new HashSet<>();
+    }
 
 }
