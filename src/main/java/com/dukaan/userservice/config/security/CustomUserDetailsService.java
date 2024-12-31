be9b7e6be0a92c02e63c1aaeac0d5f6055f66567
Aravindh-dasari <dasariaravindh21@outlook.com>
2024-12-31 20:53:40 +0530

    Added Logs to Print Privileges

--- src/main/java/com/dukaan/userservice/config/security/CustomUserDetailsService.java
+++ src/main/java/com/dukaan/userservice/config/security/CustomUserDetailsService.java
@@ -1,11 +1,14 @@
 package com.dukaan.userservice.config.security;
 
+import com.dukaan.userservice.domain.model.Role;
 import com.dukaan.userservice.domain.model.User;
 import com.dukaan.userservice.domain.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
@@ -26,6 +29,11 @@ public class CustomUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUserName(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found "+username));
+
+        Set<Role> roles = user.getRoles();
+        for(Role role:roles) {
+            System.out.println("Log loadUserByUsername Role Privileges: "+role.getPrivileges());
+        }
 
         return new CustomUserDetails(user);
     }
